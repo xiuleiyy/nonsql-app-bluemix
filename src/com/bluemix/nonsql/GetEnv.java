309cceeb6cf72229fe0be4eba0a80ca89ef84c46
acostry <zhuxiulei@gmail.com>
2014-07-11 14:25:49 +0800

    test git

--- src/com/bluemix/nonsql/GetEnv.java
+++ src/com/bluemix/nonsql/GetEnv.java
@@ -14,6 +14,7 @@ import javax.servlet.http.HttpServletResponse;
  * Servlet implementation class GenEnv
  */
 @WebServlet("/GetEnv")
+
 public class GetEnv extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
